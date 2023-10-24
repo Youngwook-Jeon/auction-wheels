89b79a3deb0f727c743ec1f1517d918c44da112c
Youngwook-Jeon <ela87@naver.com>
2023-10-24 20:30:21 +0900

    Get auction data by using synchronous communication

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -19,14 +19,12 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        using var scope = app.Services.CreateScope();
 
-            await DB.SaveAsync(items);
-        }
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
