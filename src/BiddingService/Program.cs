8150720dc044a2137ec8f3e2bfe241ce826552e2
Youngwook-Jeon <ela87@naver.com>
2023-11-15 19:10:31 +0900

    Add a background service for the auction finished event

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHostedService<CheckAuctionFinished>();
 
 var app = builder.Build();
 
