82345a75d9f6c35db0a7aa49b434177d77072753
Youngwook-Jeon <ela87@naver.com>
2023-12-06 16:54:36 +0900

    Add a toast for an auction created event

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -2,15 +2,19 @@
 
 import { useAuctionStore } from "@/hooks/useAuctionStore";
 import { useBidStore } from "@/hooks/useBidStore";
-import { Bid } from "@/types";
+import { Auction, Bid } from "@/types";
 import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { User } from "next-auth";
 import { ReactNode, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import AuctionCreatedToast from "../components/AuctionCreatedToast";
 
 type Props = {
   children: ReactNode;
+  user: User | null;
 };
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null);
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
   const addBid = useBidStore((state) => state.addBid);
@@ -31,9 +35,17 @@ export default function SignalRProvider({ children }: Props) {
         .then(() => {
           console.log("Connected to notification hub");
           connection.on("BidPlaced", (bid: Bid) => {
-            console.log("Bid placed event received");
             if (bid.bidStatus.includes("Accepted")) {
               setCurrentPrice(bid.auctionId, bid.amount);
+            }
+            addBid(bid);
+          });
+
+          connection.on("AuctionCreated", (auction: Auction) => {
+            if (user?.username !== auction.seller) {
+              return toast(<AuctionCreatedToast auction={auction} />, {
+                duration: 10000,
+              });
             }
           });
         })
